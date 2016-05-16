84f61d3ddb0734ee9cd60aa600d711782c803bda
don Trapper <cloud30@outlook.com>
2016-05-15 18:28:49 -0700

    test

---
+++ nemtWPFdraft1/ViewModel/LoginWindowModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace nemtWPFdraft1.Model
+{
+    //class LoginWindowModel : INotifyPropertyChanged
+    //{
+
+    //    private string name;
+    //    // Declare the event
+    //    public event PropertyChangedEventHandler PropertyChanged;
+    //    public LoginWindowModel()
+    //    {
+    //    }
+    //    public LoginWindowModel(string value)
+    //    {
+    //        this.name = value;
+    //    }
+    //    public string Name
+    //    {
+    //        get { return name; }
+    //        set
+    //        {
+    //            name = value;
+    //            // Call OnPropertyChanged whenever the property is updated
+    //            OnPropertyChanged(“Name”);
+    //        }
+    //    }
+    //    // Create the OnPropertyChanged method to raise the event
+    //    protected void OnPropertyChanged(string name)
+    //    {
+    //        PropertyChangedEventHandler handler = PropertyChanged;
+    //        if (handler != null)
+    //        {
+    //            handler(this, new PropertyChangedEventArgs(name));
+    //        }
+    //    }
+
+
+    //    }
+}
